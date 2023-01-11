4fce9f338745e3e7403fe26832bb91363c238f65
anderDaZu <anderda-96@hotmail.com>
2023-01-10 20:36:18 -0500

    ES6 Strings -> template literal, multi-line strings

---
+++ src/es6/02-strings.js
@@ -0,0 +1,13 @@
+let hello = 'Hello';
+let world = 'World';
+
+let epicPhrase = hello + world;
+
+// Template literals
+let epicPhrase2 = `${hello} ${world}!`;
+
+// Multi-line strings
+let lorem = 'Esto es un string \n' + 'esto es otra linea';
+
+let lorem2 = `Esta es una frase epica
+esta es la continuación de la frase epica.`;
