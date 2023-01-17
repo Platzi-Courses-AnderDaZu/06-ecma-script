44f33150424f4b3feeab223f5dd412a575447954
anderDaZu <anderda-96@hotmail.com>
2023-01-17 07:46:52 -0500

    ES10 flit to arrays and trim to strings

---
+++ src/es10/01-trimstart-trime.js
@@ -0,0 +1,7 @@
+const greeting = '    Hello!   ';
+console.log(greeting.trimStart()); // quita espacios solo al inicio
+// Hello!   
+console.log(greeting.trimEnd()); // quita espacios solo al final
+//     Hello!
+console.log(greeting.trim()); // quita espacios al incio y final
+// Hello!
